b8600167ea5cdf44521d5f435f79d74b82f3032c
Joachim <jschuler@redhat.com>
2021-02-02 15:28:44 -0500

    catalog (#13)

---
+++ build/src/app/QuickStartCatalogFederated.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from "react";
+import { QuickStartCatalogPage } from "@cloudmosaic/quickstarts";
+
+const QuickStartCatalogFederated: FunctionComponent = () => (
+  <QuickStartCatalogPage />
+);
+
+export default QuickStartCatalogFederated;
