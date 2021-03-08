2f07b50ce0073285ffca9212f7c35bf699341971
redallen <zallen@redhat.com>
2021-03-08 14:28:57 -0500

    pretty up webpack config

--- .build/webpack.dev.js
+++ .build/webpack.dev.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const webpack = require('webpack');
@@ -27,27 +26,6 @@ module.exports = merge(common('development'), {
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
       "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        include: [
-          path.resolve(__dirname, 'src'),
-          path.resolve(__dirname, 'node_modules/patternfly'),
-          path.resolve(__dirname, 'node_modules/@patternfly/patternfly'),
-          path.resolve(__dirname, 'node_modules/@patternfly/react-styles/css'),
-          path.resolve(__dirname, 'node_modules/@patternfly/react-core/dist/styles/base.css'),
-          path.resolve(__dirname, 'node_modules/@patternfly/react-core/dist/esm/@patternfly/patternfly'),
-          path.resolve(__dirname, 'node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles/css'),
-          path.resolve(__dirname, 'node_modules/@patternfly/react-table/node_modules/@patternfly/react-styles/css'),
-          path.resolve(__dirname, 'node_modules/@patternfly/react-inline-edit-extension/node_modules/@patternfly/react-styles/css'),
-          path.resolve(__dirname, 'node_modules/@patternfly/react-catalog-view-extension/dist/css'),
-          path.resolve(__dirname, 'node_modules/@cloudmosaic/quickstarts/dist/quickstarts.css')
-        ],
-        use: ["style-loader", "css-loader"]
-      }
-    ]
   },
   plugins: [
     new webpack.DefinePlugin({
--- .build/webpack.prod.js
+++ .build/webpack.prod.js
@@ -1,7 +1,5 @@
-const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserJSPlugin = require('terser-webpack-plugin');
 
@@ -14,34 +12,7 @@ module.exports = merge(common('production', { mode: "production" }, true), {
       new OptimizeCSSAssetsPlugin({})
     ],
   },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash:8].css',
-      chunkFilename: '[contenthash:8].css'
-    })
-  ],
   output: {
     filename: '[name].[contenthash:8].js'
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        include: [
-          path.resolve(__dirname, 'src'),
-          path.resolve(__dirname, 'node_modules/patternfly'),
-          path.resolve(__dirname, 'node_modules/@patternfly/patternfly'),
-          path.resolve(__dirname, 'node_modules/@patternfly/react-styles/css'),
-          path.resolve(__dirname, 'node_modules/@patternfly/react-core/dist/styles/base.css'),
-          path.resolve(__dirname, 'node_modules/@patternfly/react-core/dist/esm/@patternfly/patternfly'),
-          path.resolve(__dirname, 'node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles/css'),
-          path.resolve(__dirname, 'node_modules/@patternfly/react-table/node_modules/@patternfly/react-styles/css'),
-          path.resolve(__dirname, 'node_modules/@patternfly/react-inline-edit-extension/node_modules/@patternfly/react-styles/css'),
-          path.resolve(__dirname, 'node_modules/@patternfly/react-catalog-view-extension/dist/css'),
-          path.resolve(__dirname, 'node_modules/@cloudmosaic/quickstarts/dist/quickstarts.css')
-        ],
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      }
-    ]
-  }
 });
