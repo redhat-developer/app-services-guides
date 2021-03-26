79f72d5331e19af08c213da313ae5a3174ec57d4
Joachim Schuler <jschuler@redhat.com>
2021-03-26 17:34:33 -0400

    pass div props through to drawer

--- .build/src/app/QuickStartDrawerFederated.tsx
+++ .build/src/app/QuickStartDrawerFederated.tsx
@@ -10,11 +10,11 @@ import "@patternfly/patternfly/patternfly.min.css";
 import "@patternfly/react-catalog-view-extension/dist/css/react-catalog-view-extension.css";
 import "@cloudmosaic/quickstarts/dist/quickstarts.css";
 
-export type QuickStartDrawerFederatedProps = {
+export interface QuickStartDrawerFederatedProps extends React.HTMLProps<HTMLDivElement> {
   basePath?: string
 }
 
-const QuickStartDrawerFederated: FunctionComponent<QuickStartDrawerFederatedProps> = ({ children, basePath }) => {
+const QuickStartDrawerFederated: FunctionComponent<QuickStartDrawerFederatedProps> = ({ children, basePath, ...props }) => {
   const [activeQuickStartID, setActiveQuickStartID] = useLocalStorage(
     "quickstartId",
     ""
@@ -47,7 +47,7 @@ const QuickStartDrawerFederated: FunctionComponent<QuickStartDrawerFederatedProp
   if (allQuickStartsLoaded) {
     return (
         <QuickStartContext.Provider value={valuesForQuickstartContext}>
-          <QuickStartDrawer>{children}</QuickStartDrawer>
+          <QuickStartDrawer {...props}>{children}</QuickStartDrawer>
         </QuickStartContext.Provider>
     );
   } else {
