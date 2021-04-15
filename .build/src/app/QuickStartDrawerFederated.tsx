0b63ccffe5fde8b1192943101f336cc0b7f9cc89
Joachim <jschuler@redhat.com>
2021-04-15 11:17:37 -0400

    forward new appendTo prop to quickstart (#91)

--- .build/src/app/QuickStartDrawerFederated.tsx
+++ .build/src/app/QuickStartDrawerFederated.tsx
@@ -1,21 +1,39 @@
-import React, {useState, useEffect, FunctionComponent} from "react";
+import React, { useState, useEffect, FunctionComponent } from "react";
+import ReactDOM from "react-dom";
 import {
   QuickStartContext,
   QuickStartDrawer,
   useLocalStorage,
   useValuesForQuickStartContext,
 } from "@cloudmosaic/quickstarts";
-import {loadJSONQuickStarts} from "@app/quickstartLoader";
+import { loadJSONQuickStarts } from "@app/quickstartLoader";
 import "@patternfly/patternfly/patternfly.min.css";
 import "@patternfly/react-catalog-view-extension/dist/css/react-catalog-view-extension.css";
 import "@cloudmosaic/quickstarts/dist/quickstarts.css";
 
-export interface QuickStartDrawerFederatedProps extends React.HTMLProps<HTMLDivElement> {
-  basePath?: string
-  showDrafts?: boolean
+const getElement = (node: HTMLElement | (() => HTMLElement)) => {
+  if (typeof node === "function") {
+    return node();
+  }
+  return node;
+};
+
+export interface QuickStartDrawerFederatedProps
+  extends React.HTMLProps<HTMLDivElement> {
+  basePath?: string;
+  showDrafts?: boolean;
+  appendTo?: HTMLElement | (() => HTMLElement);
+  root?: HTMLElement | (() => HTMLElement);
 }
 
-const QuickStartDrawerFederated: FunctionComponent<QuickStartDrawerFederatedProps> = ({ children, basePath, showDrafts, ...props }) => {
+const QuickStartDrawerFederated: FunctionComponent<QuickStartDrawerFederatedProps> = ({
+  children,
+  basePath,
+  showDrafts,
+  appendTo,
+  root,
+  ...props
+}) => {
   const [activeQuickStartID, setActiveQuickStartID] = useLocalStorage(
     "quickstartId",
     ""
@@ -30,14 +48,23 @@ const QuickStartDrawerFederated: FunctionComponent<QuickStartDrawerFederatedProp
 
   useEffect(() => {
     const load = async () => {
-      const quickstarts = await loadJSONQuickStarts(basePath || "", showDrafts)
+      const quickstarts = await loadJSONQuickStarts(basePath || "", showDrafts);
       console.log(quickstarts);
       setAllQuickStarts(quickstarts);
       setAllQuickStartsLoaded(true);
-    }
+    };
     load();
-
   }, [basePath, showDrafts]);
+
+  useEffect(() => {
+    if (root) {
+      if (activeQuickStartID) {
+        getElement(root).classList.add('pf-m-expanded');
+      } else {
+        getElement(root).classList.remove('pf-m-expanded');
+      }
+    }
+  }, [root, activeQuickStartID]);
 
   const valuesForQuickstartContext = useValuesForQuickStartContext({
     activeQuickStartID,
@@ -48,14 +75,13 @@ const QuickStartDrawerFederated: FunctionComponent<QuickStartDrawerFederatedProp
   });
   if (allQuickStartsLoaded) {
     return (
-        <QuickStartContext.Provider value={valuesForQuickstartContext}>
-          <QuickStartDrawer {...props}>{children}</QuickStartDrawer>
-        </QuickStartContext.Provider>
+      <QuickStartContext.Provider value={valuesForQuickstartContext}>
+        <QuickStartDrawer appendTo={appendTo} {...props}>{children}</QuickStartDrawer>
+      </QuickStartContext.Provider>
     );
   } else {
     return <></>;
   }
-
 };
 
 export default QuickStartDrawerFederated;
