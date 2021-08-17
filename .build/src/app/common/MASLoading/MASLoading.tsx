4006eed158bfa2c4c1f390f5d7857611c8570b37
Pete Muir <pmuir@bleepbleep.org.uk>
2021-08-17 15:00:40 +0100

    fix: don’t finish loading until quickstarts a loaded, it causes flashing

---
+++ .build/src/app/common/MASLoading/MASLoading.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Bullseye, BullseyeProps, Spinner, SpinnerProps } from '@patternfly/react-core';
+
+export type MASLoadingProps = {
+  bullseyeProps?: Omit<BullseyeProps, 'children'>;
+  spinnerProps?: SpinnerProps;
+};
+
+export const MASLoading: React.FunctionComponent<MASLoadingProps> = ({
+  bullseyeProps,
+  spinnerProps,
+}: MASLoadingProps) => (
+  <Bullseye {...bullseyeProps}>
+    <Spinner {...spinnerProps} />
+  </Bullseye>
+);
