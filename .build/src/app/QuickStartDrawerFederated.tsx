3662fcf3e52993b4e635fbb0a1d273774737b987
Joachim Schuler <jdh.schuler@gmail.com>
2021-08-19 14:37:48 -0400

    premerge

--- .build/src/app/QuickStartDrawerFederated.tsx
+++ .build/src/app/QuickStartDrawerFederated.tsx
@@ -1,10 +1,14 @@
 import "@patternfly/quickstarts/dist/quickstarts.min.css";
 
-import React, { useEffect, FunctionComponent } from "react";
+import React, { useState, useEffect, FunctionComponent } from "react";
 import {
+  QuickStartContextProvider,
+  QuickStartDrawer,
   useLocalStorage,
-  QuickStartContainer,
 } from "@patternfly/quickstarts";
+import { loadJSONQuickStarts } from "@app/quickstartLoader";
+import {useAssets} from "@bf2/ui-shared";
+import {MASLoading} from "@app/common";
 
 const getElement = (node: HTMLElement | (() => HTMLElement)) => {
   if (typeof node === "function") {
@@ -22,8 +26,6 @@ export interface QuickStartDrawerFederatedProps
 
 const QuickStartDrawerFederated: FunctionComponent<QuickStartDrawerFederatedProps> = ({
   children,
-  quickStarts,
-  loading,
   showDrafts,
   appendTo,
   root,
@@ -38,6 +40,21 @@ const QuickStartDrawerFederated: FunctionComponent<QuickStartDrawerFederatedProp
     {}
   );
 
+  const [allQuickStartsLoaded, setAllQuickStartsLoaded] = useState<boolean>(false);
+  const [allQuickStarts, setAllQuickStarts] = useState<any[]>([]);
+
+  const assets = useAssets();
+
+  useEffect(() => {
+    const load = async () => {
+      const quickstarts = await loadJSONQuickStarts(assets?.getPath() || "", showDrafts);
+      console.log(quickstarts);
+      setAllQuickStarts(quickstarts);
+      setAllQuickStartsLoaded(true);
+    };
+    load();
+  }, [assets, showDrafts]);
+
   useEffect(() => {
     if (root && getElement(root)) {
       if (activeQuickStartID) {
@@ -48,19 +65,22 @@ const QuickStartDrawerFederated: FunctionComponent<QuickStartDrawerFederatedProp
     }
   }, [root, activeQuickStartID]);
 
-  const drawerProps = {
-    quickStarts,
+  const valuesForQuickstartContext = {
     activeQuickStartID,
-    allQuickStartStates,
     setActiveQuickStartID,
+    allQuickStartStates,
     setAllQuickStartStates,
-    loading,
-    appendTo,
+    allQuickStarts,
   };
-
-  return (
-    <QuickStartContainer {...drawerProps} {...props}>{children}</QuickStartContainer>
-  );
+  if (allQuickStartsLoaded) {
+    return (
+      <QuickStartContextProvider value={valuesForQuickstartContext}>
+        <QuickStartDrawer appendTo={appendTo} {...props}>{children}</QuickStartDrawer>
+      </QuickStartContextProvider>
+    );
+  } else {
+    return <MASLoading />;
+  }
 };
 
 export default QuickStartDrawerFederated;
