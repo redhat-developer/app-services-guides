b87ca59f963be41e7dc4650da7eef9325d088ffd
Joachim Schuler <jdh.schuler@gmail.com>
2021-08-19 15:02:34 -0400

    wip

--- .build/src/app/index.tsx
+++ .build/src/app/index.tsx
@@ -3,26 +3,10 @@ import QuickStartDrawerFederated from './QuickStartDrawerFederated';
 import QuickStartLoader from './quickstartLoader';
 import DemoPage from './DemoPage';
 
-export const DemoContext = React.createContext({
-  quickStarts: [],
-  loading: true
-});
-
-const App = () => {
-  const [qsState, setQsState] = React.useState({
-    quickStarts: [],
-    loading: true
-  });
-  return (
-    <>
-    <QuickStartDrawerFederated quickStarts={qsState.quickStarts} loading={qsState.loading}>
-      <DemoPage />
-    </QuickStartDrawerFederated>
-    <QuickStartLoader onLoad={(qs) => setQsState({
-      quickStarts: qs,
-      loading: false
-    })} />
-    </>
-  )};
+const App = () => (
+  <QuickStartDrawerFederated>
+    <DemoPage />
+  </QuickStartDrawerFederated>
+);
 
 export { App };
