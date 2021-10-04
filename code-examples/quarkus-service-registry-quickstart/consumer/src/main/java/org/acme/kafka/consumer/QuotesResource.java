5c8fc47d663243f1d64b04a6627c007bbf53190e
Carles Arnal <carlesarnal92@gmail.com>
2021-10-04 11:01:15 +0200

    Simplify service registry quickstart with only one consumer and one producer

--- code-examples/quarkus-service-registry-quickstart/consumer/src/main/java/org/acme/kafka/consumer/QuotesResource.java
+++ code-examples/quarkus-service-registry-quickstart/consumer/src/main/java/org/acme/kafka/consumer/QuotesResource.java
@@ -1,40 +1,19 @@
-package org.acme.kafka.producer;
+package org.acme.kafka.consumer;
 
-
-import java.util.UUID;
-import java.util.concurrent.ThreadLocalRandom;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.acme.kafka.quarkus.Quote;
 import org.eclipse.microprofile.reactive.messaging.Channel;
-import org.eclipse.microprofile.reactive.messaging.Emitter;
 
 import io.smallrye.mutiny.Multi;
 import org.jboss.resteasy.reactive.RestSseElementType;
 
 @Path("/quotes")
 public class QuotesResource {
-
-    @Channel("quote-requests")
-    Emitter<Quote> quoteRequestEmitter;
-
-    /**
-     * Endpoint to generate a new quote request id and send it to "quote-requests" Kafka topic using the emitter.
-     */
-    @POST
-    @Path("/request")
-    @Produces(MediaType.TEXT_PLAIN)
-    public Quote createRequest() {
-        UUID uuid = UUID.randomUUID();
-        Quote quote = new Quote(uuid.toString(), ThreadLocalRandom.current().nextInt());
-        quoteRequestEmitter.send(quote);
-        return quote;
-    }
 
     @Channel("quotes")
     Multi<Quote> quotes;
