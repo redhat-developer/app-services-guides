836d13b96724838c2c8b902d87e01dc6916c440d
Joachim Schuler <jdh.schuler@gmail.com>
2021-10-25 12:09:52 -0400

    fix pluralization rules when running locally

--- .build/src/app/QuickStartContainer.tsx
+++ .build/src/app/QuickStartContainer.tsx
@@ -46,6 +46,7 @@ const QuickStartContainer: FunctionComponent<QuickStartContainerProps> = ({
     setActiveQuickStartID,
     setAllQuickStartStates,
     showCardFooters: false,
+    language: 'en',
     loading: allQuickStartsLoaded,
   };
 
