e198ce86d93a798fbf0295ad1abd802f5cde0f1a
Wojciech Trocki <wtrocki@redhat.com>
2022-02-11 12:31:43 +0000

    fix: change attributes folder

--- .build/utils/attributes.js
+++ .build/utils/attributes.js
@@ -2,10 +2,10 @@ const fs = require("fs-extra");
 const path = require("path");
 const {injectAttributesIntoFileTree} = require("./common");
 
-const attributesFileName = path.join("docs", "_artifacts", "document-attributes.adoc");
+const attributesFileName = path.join("_artifacts", "document-attributes.adoc");
 
 const processAttributes = () => {
-    const rootDir = path.normalize(`${__dirname}/../../`);
+    const rootDir = path.normalize(`${__dirname}/../../docs`);
     const rawAttributes = fs.readFileSync(path.join(rootDir, attributesFileName), "utf-8").toString();
     return injectAttributesIntoFileTree(rootDir, rawAttributes);
 };
