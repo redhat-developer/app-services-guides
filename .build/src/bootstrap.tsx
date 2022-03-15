bd6e4d7c1eeb13bc829e2b85913d07f016ba588b
Joachim <jdh.schuler@gmail.com>
2022-03-15 09:12:52 -0400

    Quick starts version bump to 2.1.0 (#421)
    * bump qs version, add transform-adoc
    
    * bump transform-adoc, add styles
    
    * bump to 2.1.0
    
    * use npm 16.6.2
    
    Co-authored-by: Cooper Trowbridge <ctrowbri@redhat.com>

--- .build/src/bootstrap.tsx
+++ .build/src/bootstrap.tsx
@@ -4,5 +4,6 @@ import { App } from './app';
 import '@patternfly/patternfly/patternfly.css';
 import '@patternfly/patternfly/patternfly-addons.css';
 import "@patternfly/quickstarts/dist/quickstarts.min.css";
+import "@patternfly/transform-adoc/dist/transform-adoc.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
